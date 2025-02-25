4e0791b75bfb5e1a61ee5ba7a7326e5716440498
PedroHenriquePH <pedrohenriquemendes014@gmail.com>
2025-02-25 10:54:30 -0300

    meu primeiro commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let currentIndex = 0;
+const images = document.querySelectorAll('.carousel-images img');
+const totalImages = images.length;
+
+function showNextImage() {
+    images[currentIndex].style.display = 'none';
+    currentIndex = (currentIndex + 1) % totalImages;
+    images[currentIndex].style.display = 'block';
+}
+
+setInterval(showNextImage, 5000);
+showNextImage();
